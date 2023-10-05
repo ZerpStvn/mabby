096d166a6f114cdada63c1e5f8d7dbd2e4f971d1
steveGFW <steven.gfw2023@gmail.com>
2023-10-05 15:29:04 +0800

    update

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
   var text =
-    "  Hi mabby, as I develop this website I want to write you this letter on our 2nd monthsary, my heart is filled with an overwhelming sense of gratitude and love. Time has flown by, yet every moment with you feels like a cherished eternity. I want you to know just how much you mean to me and how you inspire me every day. deeply sorry I am for making you worry and cry. Knowing that I've caused you pain and concern breaks my heart, and I want you to understand that it was never my intention. My love for you is unwavering, and I am committed to making things right and ensuring that you never have to feel that way again.  I promise to cherish and adore you, not just on our monthsary, but every single day. Thank you for being my inspiration, my rock, and the love of my life. I am truly blessed to have you by my side, and I can't wait to see where our love takes us in the future.";
+    "They say that a successful relationship is built on trust, understanding, and the ability to share a pizza without any arguments. Well, we've certainly aced the pizza part, but I promise to work on the rest. I promise to cherish and adore you, not just on our monthsary, but every single day. Thank you for being my inspiration, my rock, and the love of my life. I am truly blessed to have you by my side, and I can't wait to see where our love takes us in the future.";
   var delay = 50; // Adjust the delay (in milliseconds) between each character
 
   function typeWriter(text, i) {
